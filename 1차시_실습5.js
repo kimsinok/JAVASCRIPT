c38e64a041c7b9fa5965783f8784da4ea8cbbf8a
kimsinok <javaenjoy0714@gmail.com>
2023-01-09 15:28:23 +0900

    Create 1차시_실습5.js

---
+++ 1차시_실습5.js
@@ -0,0 +1,17 @@
+// Template Literals
+// 멀티라인 문자열
+
+let str = 'Hello!!\nHi!!\nGoodBye!!';
+
+console.log(str);
+
+// Template literals
+
+str = `
+     Hello!!
+     Hi!!
+     GoodBye~~
+`;
+
+
+console.log(str);
