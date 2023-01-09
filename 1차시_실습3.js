5eed6ca04c95e296cd8c58d0c7b2517525a4a8e8
kimsinok <javaenjoy0714@gmail.com>
2023-01-09 14:49:53 +0900

    Create 1차시_실습3.js

---
+++ 1차시_실습3.js
@@ -0,0 +1,19 @@
+
+
+// 객체 리터럴을 이용한 객체 생성
+
+const member = {
+     name: 'Hong gil dong' ,
+     age: 20
+};
+
+
+console.log(typeof member);   // object
+
+console.log(member.name);     // Hong gil dong
+
+// console.log(member['name']);
+
+// console.log(member.age);
+
+console.log(member['age']);   // 20
