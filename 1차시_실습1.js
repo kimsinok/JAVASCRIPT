b707d1fcb77466d3469fd5faa5410085bb919cd7
kimsinok <javaenjoy0714@gmail.com>
2023-01-09 14:39:52 +0900

    Create 1차시_실습1.js

---
+++ 1차시_실습1.js
@@ -0,0 +1,25 @@
+var str = 'Hello, javascript';
+
+var str = 'Hi, javascript';
+
+
+console.log(str);  // Hi, javascript
+
+
+let userName = 'Julia';
+
+// let userName = 'Brad';  // SyntaxError: Identifier 'userName' has already been declared
+
+userName = 'Angelina';
+
+console.log(userName);
+
+
+const PI = 3.14;
+
+// PI = 3.141592;  // TypeError: Assignment to constant variable.
+
+console.log(PI); // 3.14
+
+
+
