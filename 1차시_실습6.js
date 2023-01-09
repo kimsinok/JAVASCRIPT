214542033d0224f0204635e0f3afe1af71ea19d4
kimsinok <javaenjoy0714@gmail.com>
2023-01-09 15:33:17 +0900

    Create 1차시_실습6.js

---
+++ 1차시_실습6.js
@@ -0,0 +1,11 @@
+
+// Template literals 
+// 표현식 삽입법
+
+const firstName = 'Gil-dong';
+
+const lastName = 'Hong';
+
+console.log('My name is ' + firstName + ' ' + lastName);
+
+console.log(`My name is ${firstName} ${lastName}`);
