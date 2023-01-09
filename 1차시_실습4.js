8d1273279f55b74d5728458d654c8ac4e9f73327
kimsinok <javaenjoy0714@gmail.com>
2023-01-09 14:54:08 +0900

    Create 1차시_실습4.js

---
+++ 1차시_실습4.js
@@ -0,0 +1,27 @@
+// 기본형과 참조형의 차이
+
+let a = 1;
+
+let b = a;
+
+console.log(b);  // 1
+
+b = 2;
+
+console.log(a);  // 1
+
+console.log(b);  // 2
+
+
+let src = { x: 1 };
+
+let copy = src;
+
+console.log(copy.x);  // 1
+
+copy.x = 10;
+
+
+console.log(src.x);  // 10
+
+console.log(copy.x);  // 10
