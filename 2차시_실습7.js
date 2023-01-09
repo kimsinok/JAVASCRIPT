a5c05c754c070367d18987d855a4df4801892e1e
kimsinok <javaenjoy0714@gmail.com>
2023-01-09 17:07:02 +0900

    Create 2차시_실습7.js

---
+++ 2차시_실습7.js
@@ -0,0 +1,38 @@
+
+const increase = function (num) {
+     return ++num;
+}
+
+
+const decrease = function (num) {
+     return --num;
+}
+
+
+const auxs = {
+     increase,  // increase: increase
+     decrease
+};
+
+
+
+// 함수를 함수의 매개변수로 전달할 수 있고 
+// 함수를 함수의 반환값으로 사용할 수 있다.
+function makeCounter(func) {
+
+     let num = 0;
+
+     return function() {
+          num = func(num);
+          return num;
+     };
+
+}
+
+
+const inc = makeCounter(auxs.increase);
+
+console.log(inc());  // 1
+console.log(inc());  // 2
+console.log(inc());  // 3
+console.log(inc());  // 4
