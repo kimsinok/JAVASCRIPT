7cb4c0712fe85ebac010378d9581cc540bdd66d2
kimsinok <javaenjoy0714@gmail.com>
2023-01-09 14:45:08 +0900

    Create 1차시_실습2.js

---
+++ 1차시_실습2.js
@@ -0,0 +1,24 @@
+
+let isMarried = false; 
+
+let userName = '홍길동';
+
+let age = 20;
+
+let height = 180.5;
+
+let gender;
+
+let member = null;
+
+console.log(typeof isMarried); // boolean
+
+console.log(typeof userName); // string
+
+console.log(typeof age);   // number
+
+console.log(typeof height);  // number
+
+console.log(typeof gender);   // undefined
+
+console.log(typeof member);   // object 
