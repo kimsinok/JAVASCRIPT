6fd3f29bc7581914c811a7532d5dd9f69c256edc
kimsinok <javaenjoy0714@gmail.com>
2023-01-09 16:51:52 +0900

    중첩함수

---
+++ 2차시_실습5.js
@@ -0,0 +1,24 @@
+
+const add = function (x, y) {
+     return x + y;
+}
+
+const sub = function (x, y) {
+     return x - y;
+}
+
+
+const mul = function (x, y) {
+     return x * y;
+}
+
+const calcurate = function (a, b, func) {
+     console.log(func(a, b));
+}
+
+
+calcurate(1, 2, add);  // 3
+
+calcurate(1, 2, sub);  // -1
+
+calcurate(1, 2, mul);  // 2
