1ed1ce6efc1c87370288aca7353bdc986db472cd
kimsinok <javaenjoy0714@gmail.com>
2023-01-10 16:30:51 +0900

    const 키워드

---
+++ 4차시_실습8.js
@@ -0,0 +1,11 @@
+
+{
+     console.log(foo); // ReferenceError: Cannot access 'foo' before initialization
+     const foo = 1;
+     console.log(foo);
+
+}
+
+console.log(foo); // ReferenceError: Cannot access 'foo' before initialization
+
+
