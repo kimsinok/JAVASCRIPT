a19a51aa1daed0e4fd4447d9e1265e5ac7bfe00a
kimsinok <javaenjoy0714@gmail.com>
2023-01-10 15:51:04 +0900

    블록 레벨 스코프

---
+++ 4차시_실습4.js
@@ -0,0 +1,20 @@
+
+// 블록 레벨 스코프
+
+console.log(x);   // undefined
+
+var x = 1;
+
+console.log(x);   // 1
+
+
+/*
+     var x = undefined;
+
+     console.log(x);
+
+     x = 1;
+
+     console.log(x);
+     
+*/
