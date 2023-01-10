726561b58d9d6ff8fe5d2266f283b71baaeb8f34
kimsinok <javaenjoy0714@gmail.com>
2023-01-10 15:56:33 +0900

    var 키워드로 선언된 변수의 문제점

---
+++ 4차시_실습3.js
@@ -0,0 +1,22 @@
+
+// var 키워드로 선언된 변수의 문제점
+
+
+console.log(x);   // undefined
+
+var x = 1;
+
+console.log(x);   // 1
+
+
+/*
+     var x = undefined;
+
+     console.log(x);
+
+     x = 1;
+
+     console.log(x);
+*/
+
+
