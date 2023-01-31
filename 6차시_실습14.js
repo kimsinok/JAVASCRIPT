22fca5402bd613b3c449ab21a33545b65b96a77b
kimsinok <javaenjoy0714@gmail.com>
2023-01-31 15:30:54 +0900

    메소드 축약 표현(ES6)

---
+++ 6차시_실습14.js
@@ -0,0 +1,16 @@
+
+
+// 메소드 축약 표현 (ES6)
+
+const person = {
+
+     name: 'Kim' ,
+
+     sayHello () {
+          console.log(`Hello, ${this.name}`);
+     }
+
+}
+
+
+person.sayHello();  // Hello, Kim
