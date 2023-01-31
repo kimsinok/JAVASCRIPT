cdd1f290eb2730ec573b34ccfccff2ea800f25cc
kimsinok <javaenjoy0714@gmail.com>
2023-01-31 10:38:35 +0900

    변수 호이스팅에 대한 예제

---
+++ 1차시_실습1_1.js
@@ -0,0 +1,20 @@
+//변수 호이스팅에 대한 예제
+
+console.log(num);  
+
+var num = 1;
+
+console.log(num);
+
+
+/*
+     // 변수 호이스팅
+     var num;              
+
+     console.log(num); // undefined
+
+     num = 1;
+
+     console.log(num);  // 1
+
+*/
