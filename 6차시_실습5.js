99fd584a7289ec1eb73b2e264b7a90ddbdf7b4ea
kimsinok <javaenjoy0714@gmail.com>
2023-01-31 15:18:30 +0900

    프로퍼티 접근

---
+++ 6차시_실습5.js
@@ -0,0 +1,18 @@
+/*
+ * 프로퍼티 접근
+ */
+
+
+const person = {
+
+     name: 'Lee'
+
+}
+
+
+// 마침표 표기법을 이용한 프로퍼티 접근
+console.log(person.name);
+
+// 대괄호 표기법을 이용한 프로퍼티 접근
+console.log(person['name']);
+
