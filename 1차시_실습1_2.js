9b2b9ef19dd70da3052b05c8863bc7f0886fcca5
kimsinok <javaenjoy0714@gmail.com>
2023-01-31 10:39:26 +0900

    변수 호이스팅에 대한 예제

---
+++ 1차시_실습1_2.js
@@ -0,0 +1,22 @@
+/*
+ * 변수 호이스팅에 대한 예제
+ */
+
+console.log(num); 
+
+let num = 1;
+
+console.log(num);
+
+
+/*
+
+let num;
+
+console.log(num); // ReferenceError: Cannot access 'num' before initialization
+
+num = 1; 
+
+console.log(num);
+
+*/
