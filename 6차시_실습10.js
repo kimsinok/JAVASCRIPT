3c3d76326517051040efb957a6c98132d65f978c
kimsinok <javaenjoy0714@gmail.com>
2023-01-31 15:22:51 +0900

    프로퍼티 축약 표현(ES6)

---
+++ 6차시_실습10.js
@@ -0,0 +1,15 @@
+
+// ES6
+// 프로퍼티 축약 표현 (property shorthand)
+
+let userName = 'Hong';
+let age = 20;
+
+const person = {
+     userName,
+     age
+};
+
+console.log(person.userName);   // Hong
+
+
